fb879da0223563148e0d1eb23ee961983ed1b744
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2024-02-05 17:56:47 +0100

    Add get method

--- index.js
+++ index.js
@@ -10,22 +10,42 @@ class hashMap {
     this.bucket = new Array(16).fill(null);
     this.length = this.bucket.length;
   }
+  checker(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+  }
   hashkey(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + (key.charCodeAt(i) % this.length);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.length;
     }
 
     return hashCode;
   }
   set(key, value) {
-    let arrayEl = this.bucket[hashCode(key)];
+    this.checker(this.hashkey(key));
     let newNode = new node(key, value);
-    arrayEl = newNode;
+    this.bucket[this.hashkey(key)] = newNode;
+  }
+  get(key) {
+    let arrayEl = this.bucket[this.hashkey(key)];
+    if (arrayEl == null) {
+      return null;
+    } else if (arrayEl.key == key) return arrayEl.value;
+    else {
+      let temp = arrayEl;
+      while (temp != null) {
+        if (temp.key == key) {
+          return temp.value;
+        }
+        temp = temp.nextNode;
+      }
+      return null;
+    }
   }
 }
 const newMap = new hashMap();
-console.log(newMap.hashkey("AYMEN"));
-console.log(newMap.hashkey("AYMEN") % 16);
+newMap.set("Aymen", "Loudiy");
